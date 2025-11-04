8addc194c83d39c5274787f0a704ab6003220d38
MathssF <102183898+MathssF@users.noreply.github.com>
2025-11-04 23:36:40 +0000

    more

---
+++ src/utils/players.ts
@@ -0,0 +1,10 @@
+import Match, { MatchDetails } from "@/interfaces/matchs";
+import PlayerMatch from "@/interfaces/players";
+
+export function selectMatch(matchs: Match[], id: number) {
+  
+}
+
+export function matchDetails(match: Match, players: PlayerMatch) { // : MatchDetails {
+  
+}

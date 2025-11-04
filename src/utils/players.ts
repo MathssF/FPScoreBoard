b939eb9a8aae53ed55f768181cbc1b3b7e5eb4bb
MathssF <102183898+MathssF@users.noreply.github.com>
2025-11-04 23:38:38 +0000

    more

--- src/utils/players.ts
+++ src/utils/players.ts
@@ -1,8 +1,10 @@
 import Match, { MatchDetails } from "@/interfaces/matchs";
 import PlayerMatch from "@/interfaces/players";
 
-export function selectMatch(matchs: Match[], id: number) {
-  
+export function selectMatch(matches: Match[], id: number): Match | undefined {
+  return matches.find((elem) => {
+    elem.matchid === id;
+  })
 }
 
 export function matchDetails(match: Match, players: PlayerMatch) { // : MatchDetails {

dc4dbef69b4086d1cb51b947b4cadd4292cb9832
MathssF <102183898+MathssF@users.noreply.github.com>
2026-04-13 21:40:18 +0000

    Retirando os imports e os comentários do contexto de Matches

--- src/context/matchs.tsx
+++ src/context/matchs.tsx
@@ -2,18 +2,7 @@
 
 import { createContext, useContext, useEffect, useState, ReactNode } from "react";
 import Match from "@/interfaces/matchs";
-import { MatchDetails } from "@/interfaces/matchs";
-import { makeMatchDetails } from "@/utils/matchs";
-import PlayerMatch from "@/interfaces/players";
-import MapStats from "@/interfaces/maps";
 
-/*
-interface MatchContextType {
-  matches: Match[];
-  fetchMatches: () => Promise<void>;
-  checkM: boolean;
-}
-*/
 
 interface MatchContextType {
   matches: Match[];
@@ -24,53 +13,6 @@ interface MatchContextType {
 
 const MatchContext = createContext<MatchContextType | undefined>(undefined);
 
-/*
-export function MatchProvider({ children }: { children: ReactNode }) {
-  const [matches, setMatches] = useState<Match[]>([]);
-  const [checkM, setCheckM] = useState<boolean>(false);
-  const [matchId, setMatchId] = useState<Match | null>(null);
-
-  const [allMatchsDetails, setAllMatchsDetails] = useState<MatchDetails[]>([])
-  const [matchDetail, setMatchDetail] = useState<MatchDetails | null>(null);
-
-  async function makeDetails(
-    match: Match[],
-    players: PlayerMatch[],
-    maps: MapStats[]
-  ) {
-    const list = match.map((elem) => {
-      makeMatchDetails(elem, players, maps);
-    })
-    return list;
-  }
-
-  
-  async function fetchMatches() {
-    try {
-      const res = await fetch("/api/matchs", { cache: "no-store" });
-      const data = await res.json();
-      setCheckM(true);
-      setMatches(data.matches || []);
-    } catch (error) {
-      console.error("Error: Matchs not found:", error);
-      setCheckM(true);
-      setMatches([]);
-  
-    }
-  }
-
-  useEffect(() => {
-  fetchMatches();
-  }, []);
-
-
-  return (
-    <MatchContext.Provider value={{ matches, fetchMatches, checkM }}>
-      {children}
-    </MatchContext.Provider>
-  );
-}
-*/
 
 export function MatchProvider({ children }: { children: ReactNode }) {
   const [matches, setMatches] = useState<Match[]>([]);

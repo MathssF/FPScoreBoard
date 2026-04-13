59411f29f995e62612a0b4a10c2fe0a5631d9405
MathssF <102183898+MathssF@users.noreply.github.com>
2026-04-13 22:13:17 +0000

    Players, Context e page

--- src/app/dashboard/players-matchs/page.tsx
+++ src/app/dashboard/players-matchs/page.tsx
@@ -25,17 +25,6 @@ export default function PlayersMatchesPage() {
     });
   }, [playerMatches, playerFilter, matchFilter]);
 
-  /*
-  if (!checkP) {
-    return (
-      <div className="flex items-center justify-center min-h-screen bg-zinc-900 text-zinc-100">
-        <Header />
-        <LangFlags />
-        <p>Carregando dados dos jogadores...</p>
-      </div>
-    );
-  }
-    */
 
   if (loading) {
     return (

e8320ce6cfea4562974175f9b0a086ec24c10697
MathssF <102183898+MathssF@users.noreply.github.com>
2026-04-16 01:22:31 +0000

    more

--- src/app/dashboard/player/[steamid]/page.tsx
+++ src/app/dashboard/player/[steamid]/page.tsx
@@ -21,17 +21,6 @@ export default function PlayerDetailsPage() {
     return playerMatches.filter((m) => m.steamid64 === player.steamid64);
   }, [playerMatches, player]);
 
-  /*
-  if (!checkP) {
-    return (
-      <div className="flex items-center justify-center min-h-screen bg-zinc-900 text-zinc-100">
-        <Header />
-        <LangFlags />
-        <p>Carregando informações do jogador...</p>
-      </div>
-    );
-  }
-  */
 
   if (loading) {
     return (
